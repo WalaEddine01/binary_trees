0d3860e21c888eb9e7951aae403b8989a1418adc
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-12-01 02:21:16 +0100

    update task 0

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,23 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *left_child;
+	binary_tree_t *new;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
-	left_child = binary_tree_node(parent, value);
-	if (left_child == NULL)
-	{
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
-	}
+
 	if (parent->left != NULL)
 	{
-		left_child->left = parent->left;
-		parent->left->parent = left_child;
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
-	parent->left = left_child;
-	return (left_child);
-}
+	parent->left = new;
 
+	return (new);
+}
