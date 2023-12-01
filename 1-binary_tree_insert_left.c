b09b8a167c66dfed109de2a9c2ab3ce000d6c571
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-01 22:15:48 +0100

    I made the changes on 2023-12-01 22:15:48

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,10 +10,11 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = binary_tree_node(parent, value);
+	binary_tree_t *new;
 
 	if (parent == NULL)
 		return (NULL);
+    new = binary_tree_node(parent, value);
 
 	if (new == NULL)
 		return (NULL);
