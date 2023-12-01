3df5845ca9327c3aa5748574ba6fcc6b380e6b05
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-12-01 02:02:18 +0100

    complet task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,5 +8,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    
+    binary_tree_t *root = malloc(sizeof(binary_tree_t));
+
+    if (root == NULL)
+	    return (NULL);
+    root->n = value;
+    root->parent = parent;
+    root->left = NULL;
+    root->right = NULL;
+    return (root);
 }
+
