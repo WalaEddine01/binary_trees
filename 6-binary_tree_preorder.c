bf409591934a1adbc5fed8adb15015dc763ba8c4
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-12-02 00:14:11 +0100

    update task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,8 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_preorder(tree->left);
-	binary_tree_preorder(tree->right);
-	func(tree->n);
+	binary_tree_preorder(tree->left, func(tree->left->n));
+	binary_tree_preorder(tree->right, func(tree->right->n));
+	binary_tree_preorder(tree, func(tree->n));
 }
 
