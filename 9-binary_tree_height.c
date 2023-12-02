b0145ec35cc8710589c4e0c6dda736577226a7b3
Nadjib-coder <nadjibmouhoun1@gmail.com>
2023-12-02 01:32:44 +0100

    complet task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (MAX(left_height, right_height) + 1);
+	return (fmax(left_height, right_height) + 1);
 }
 
--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
  * struct binary_tree_s - Binary tree node
