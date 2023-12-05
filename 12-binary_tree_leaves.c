18586f232aa2313c81a4c4f223ad90cb03424dcd
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-05 17:51:49 +0100

    complet task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL)
-		return(0);
+		return (0);
 	count += (tree->left == NULL && tree->right == NULL) ? 1 : 0;
 	count += binary_tree_leaves(tree->left);
 	count += binary_tree_leaves(tree->right);
