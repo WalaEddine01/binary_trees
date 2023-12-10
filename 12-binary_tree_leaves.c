f42f0cf1de3c9edd53afc520a8968dda11726504
Nadjib-coder <nadjib.programmation1@gmail.com>
2023-12-10 15:27:24 +0100

    solve betty style in task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * size_t binary_tree_leaves - a function that counts the leaves in a binary tree.
+ * binary_tree_leaves - a function that counts the leaves in a binary tree.
  * @tree: is a pointer to the root node of the tree .
  * Return: the number of leaves,Otherwise 0.
  */
